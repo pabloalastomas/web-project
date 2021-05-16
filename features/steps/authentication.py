8722958d9e502334ef4b6a4da3548d9c0bf3e4cb
sergio <sergiobeltranguerrero@gmail.com>
2021-05-16 10:04:07 +0200

    auth feature

--- features/steps/authentication.py
+++ features/steps/authentication.py
@@ -1,13 +1,25 @@
 from behave import *
+from django.contrib.auth.models import User
 
 use_step_matcher("parse")
 
 
 @given('Exists a user "{user}" with password "{password}"')
 def step_impl(context, user, password):
-    from django.contrib.auth.models import User
-    User.objects.create_user(username=user, email='user@example.com', password=password)
+    # User.objects.create_user(username=user, email='user@user.com', password=password)
+    context.browser.visit('http://localhost:8000/register/')
+    context.browser.fill("username", user)
+    context.browser.fill("password1", password)
+    context.browser.fill("password2", password)
+    context.browser.find_by_value('Create my account').click()
 
-@given('I login as user "user" with password "password"')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given I login as user "user" with password "password"')
+
+@given('I login as user "{user}" with password "{password}"')
+def step_impl(context, user, password):
+    context.browser.visit('http://localhost:8000/')
+    form = context.browser.find_by_css('.login-form')
+    context.browser.fill("username", user)
+    context.browser.fill("password", password)
+
+    form.find_by_value('Log-in').click()
+    assert context.browser.is_text_present('User Profile')
--- features/update_status.feature
+++ features/update_status.feature
@@ -3,14 +3,14 @@ In order to keep track of what content I want to see and what I have seen,
 As a user
 I want to update the status of the content.
     Background: There is a registered user
-    Given Exists a user "user" with password "password"
+    Given Exists a user "user" with password "webproject"
 
-#  Scenario: Update the content status
-#    Given I login as user "user" with password "password"
-#    When I update status
-#      | status      |
-#      | Watching    |
-#    Then I'm viewing the status update for content by "user"
-#      | status      |
-#      | Watching    |
-#    And The content has a status
+  Scenario: Update the content status
+    Given I login as user "user" with password "webproject"
+    When I update status
+      | status      |
+      | Watching    |
+    Then I'm viewing the status update for content by "user"
+      | status      |
+      | Watching    |
+    And The content has a status

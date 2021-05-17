f50aa3d518ae9444a8e35c0e67340936f0339c5c
sergio <sergiobeltranguerrero@gmail.com>
2021-05-17 17:16:09 +0200

    features

--- features/steps/authentication.py
+++ features/steps/authentication.py
@@ -7,12 +7,6 @@ use_step_matcher("parse")
 def step_impl(context, user, password):
     from django.contrib.auth.models import User
     User.objects.create_user(username=user, email='user@user.com', password=password)
-    # context.browser.visit('http://localhost:8000/register/')
-    # context.browser.fill("username", user)
-    # context.browser.fill("password1", password)
-    # context.browser.fill("password2", password)
-    # context.browser.find_by_value('Create my account').click()
-
 
 @given('I login as user "{user}" with password "{password}"')
 def step_impl(context, user, password):

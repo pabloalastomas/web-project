5f6c1ba6216b037734b39589a47e5dfb2235175b
sala1714 <pepesalamarques@gmail.com>
2021-05-19 13:32:02 +0200

    Settings allowed hosts modified

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '+43705!-og=^x!3&e#du=tn(khz*=vk!)$ury#6j+sbyym%(!7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["web-project-entertainment-db.herokuapp.com"]
 
 # Application definition
 

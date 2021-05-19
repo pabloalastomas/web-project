239cee6376610a6ca7906ec82f2b5a1dbc19bb84
sergio <sergiobeltranguerrero@gmail.com>
2021-05-19 10:17:26 +0200

    added create content feature

--- features/steps/create_content.py
+++ features/steps/create_content.py
@@ -1,7 +1,7 @@
 import time
 
 from behave import *
-from entertainment_db.models import StatusUserContent, Content
+from entertainment_db.models import *
 
 use_step_matcher("parse")
 
@@ -64,4 +64,63 @@ def step_impl(context, username):
         content = Content.objects.get(title=row['film'])
         status_user_content = StatusUserContent.objects.get(content=content, user=user)
         is_update = is_update and row['review'] == status_user_content.review
+    assert is_update
+
+
+@when("I create the rating")
+def step_impl(context):
+    for row in context.table:
+        context.browser.visit(context.get_url('/profile'))
+        context.browser.find_by_id('select2-search_bar-container').click()
+        context.browser.find_by_css('input.select2-search__field').fill(row['film'])
+        context.browser.find_by_css('.select2-result-repository__title').click()
+        context.browser.find_by_id('search_button').click()
+        context.browser.find_by_id('id_type').find_by_value(status_values[row['status']]).click()
+        context.browser.find_by_value('Save').click()
+        context.browser.find_by_css(f'span[data-value="{row["rating"]}"]').click()
+        context.browser.find_by_value('Save').click()
+
+
+@then('I\'m viewing the rating created for content by "{username}"')
+def step_impl(context, username):
+    from django.contrib.auth.models import User
+    user = User.objects.get(username=username)
+    is_update = True
+    for row in context.table:
+        content = Content.objects.get(title=row['film'])
+        assessment = Assessment.objects.get(user=user, content=content)
+        is_update = is_update and assessment.rating == int(row['rating'])
+    assert is_update
+
+
+@when("Exists streaming platforms")
+def step_impl(context):
+    for row in context.table:
+        StreamingPlatforms.objects.create(name=row["platform"])
+
+
+@then("I create the platform link")
+def step_impl(context):
+    for row in context.table:
+        context.browser.visit(context.get_url('/profile'))
+        context.browser.find_by_id('select2-search_bar-container').click()
+        context.browser.find_by_css('input.select2-search__field').fill(row['film'])
+        context.browser.find_by_css('.select2-result-repository__title').click()
+        context.browser.find_by_id('search_button').click()
+        context.browser.find_by_text('Add Link').click()
+        context.browser.find_by_id('id_platform').find_by_text(row['platform']).click()
+        context.browser.fill("url", row['link'])
+        context.browser.find_by_value('Save').click()
+
+
+@then('I\'m viewing the platform link created for content by "{username}"')
+def step_impl(context, username):
+    from django.contrib.auth.models import User
+    user = User.objects.get(username=username)
+    is_update = True
+    for row in context.table:
+        content = Content.objects.get(title=row['film'])
+        platform = StreamingPlatforms.objects.get(name=row['platform'])
+        platform_content = PlatformContent.objects.get(user=user, content=content, platform=platform)
+        is_update = is_update and platform_content.url == row['link']
     assert is_update

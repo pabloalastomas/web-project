06124e5e0c74da657bf214ba3070b0fe6442bed2
sergio <sergiobeltranguerrero@gmail.com>
2021-05-20 16:35:41 +0200

    view content feature

--- features/steps/view_content.py
+++ features/steps/view_content.py
@@ -5,7 +5,11 @@ use_step_matcher("parse")
 
 @then('I\'m viewing the list of content')
 def step_impl(context):
-    context.browser.visit(context.get_url('/profile'))
+    is_presents = True
+    for row in context.table:
+        context.browser.visit(context.get_url('/profile'))
+        is_presents = is_presents and context.browser.is_text_present(row['film'])
+    assert is_presents
 
 
 @then('I\'m viewing the global rating for the content')

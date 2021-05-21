551a9b9f70ec1bd35ef1db83c275e9a9ac0ceb37
sergio <sergiobeltranguerrero@gmail.com>
2021-05-21 13:00:01 +0200

    delete content feature repaired

--- features/environment.py
+++ features/environment.py
@@ -3,6 +3,7 @@ from splinter.browser import Browser
 
 def before_all(context):
     context.browser = Browser('chrome', headless=True)
+    context.browser.driver.set_window_size(1200, 985)
 
 
 def after_all(context):
--- features/steps/delete_content.py
+++ features/steps/delete_content.py
@@ -1,3 +1,5 @@
+import time
+
 from behave import *
 from entertainment_db.models import *
 
